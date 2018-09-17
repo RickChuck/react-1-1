b6f93c0786a8c898c33f97eaa40d3e2589138d50
RickChuck <you@example.com>
2018-09-17 09:09:24 -0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,46 +1,108 @@
 import React, { Component } from "react";
+import "./App.css";
 
 export default class App extends Component {
-    constructor() {
-        super();
-        this.state() = {
-            products: [
-                {
-                    id: 1,
-                    imageUrl: "",
-                    title:"",
-                    price:12.99,
-                    description:""
-                },
-                {
-                    id: 2,
-                    imageUrl: "",
-                    title:"",
-                    price:8.99,
-                    description:""
-                },
-                {
-                    id: 2,
-                    imageUrl: "",
-                    title:"",
-                    price:5.99,
-                    description:""
-                },
-            ],
-            cart: []
-        };
+  constructor() {
+    super();
+    this.state = {
+        cart: [],
+        hats: [
+            {
+                id: 1,
+                tile: "Big 'ol Hat",
+                description: "Get this... It's a big hat",
+                price: 47.98,
+                imageUrl: "https://via.placeholder.com/150x150"
+            },
+            {
+                id: 2,
+                title: "Adamantium hat",
+                description: "Ya'll already know",
+                price: 11112.99,
+                imageUrl: "https://via.placeholder.com/150x150"
+            }
+        ],
+        beachGear: [
+            {
+                id: 3,
+                title: "Tent",
+                description: "For sleeping, but outside.",
+                price: 32.99,
+                imageUrl: "https://via.placeholder.com/150x150"
+            }
+        ]
+      };
     }
-    render(){
-        return(
-            <div className="App">
-             <section className="products">
-              <h1>Products</h1>
-             </section>
 
-             <section className="cart">
+    addToCart(item) {
+        this.setState({
+          cart: [...this.state.cart, item]
+        });
+      }
+    
+      checkout = () => {
+        this.setState({ cart: [] });
+        alert("Purchase is complete!");
+      };
+    
+      render() {
+        return (
+          <div className="App">
+            <section className="products">
+              <h1>Products</h1>
+              <h2>Hats</h2>
+              {this.state.hats.map(item => (
+                <div key={item.id} className="product">
+                  <img src={item.imageUrl} />
+                  <div className="product-info">
+                    <h4>{item.title}</h4>
+                    <p>{item.description}</p>
+                    <p>{item.price}</p>
+                    <button onClick={() => this.addToCart(item)}>
+                      Add to Cart
+                    </button>
+                  </div>
+                </div>
+              ))}
+    
+              <h2>Beach Gear</h2>
+              {this.state.beachGear.map(item => (
+                <div key={item.id} className="product">
+                  <img src={item.imageUrl} />
+                  <div className="product-info">
+                    <h4>{item.title}</h4>
+                    <p>{item.description}</p>
+                    <p>{item.price}</p>
+                    <button onClick={() => this.addToCart(item)}>
+                      Add to Cart
+                    </button>
+                  </div>
+                </div>
+              ))}
+            </section>
+    
+            <section className="cart">
               <h1>Cart</h1>
-             </section>
-            </div>
-        )
+              <h2>
+                Total: $
+                {this.state.cart.reduce(
+                  (totalPrice, product) => (totalPrice += product.price),
+                  0
+                )}
+              </h2>
+              <button onClick={this.checkout}>Checkout</button>
+              {this.state.cart.map(item => (
+                <div key={item.id} className="product">
+                  <img src={item.imageUrl} />
+                  <div className="product-info">
+                    <h4>{item.title}</h4>
+                    <p>{item.description}</p>
+                    <p>{item.price}</p>
+                  </div>
+                </div>
+              ))}
+            </section>
+          </div>
+        );
+      }
     }
-}
